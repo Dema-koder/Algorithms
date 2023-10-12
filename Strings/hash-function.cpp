a6ed2689ccb272406ef1e67901b32be71bcc2c95
Dema-koder <74598261+Dema-koder@users.noreply.github.com>
2023-10-12 13:08:52 +0300

    Create hash-function.cpp
    Вычисление хэша для строки за линию

---
+++ Strings/hash-function.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+typedef long long ll;
+
+const int P = 31;
+const ll MOD = 1e9 + 7;
+
+// вычисление хэша для строки за линию
+ll p_pow = 1;
+ll hashFromString(string s) {
+	ll hash = 0;
+	for (int i = 0; i < s.size(); i++) {
+		hash += (s[i] - 'a' + 1) * p_pow;
+		hash %= MOD;
+		p_pow *= P;
+		p_pow% MOD;
+	}
+	return hash;
+}
+
+int main() {
+	string s; cin >> s;
+	cout << hashFromString(s);
+}
