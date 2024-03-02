8a5b236f7ce8ffc02c27ba24a8f6e8c8d0b8914f
Dema-koder <74598261+Dema-koder@users.noreply.github.com>
2024-03-02 15:21:53 +0300

    Create D. Призы.cpp

---
+++ Math-2/D. Призы.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n,m;
+    cin >> n >> m;
+
+    cout << n/__gcd(n,m)<<endl;
+
+    return 0;
+}
