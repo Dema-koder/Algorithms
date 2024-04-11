7922913d289a769710fa47f6db0d3e83b40a5947
Dema-koder <74598261+Dema-koder@users.noreply.github.com>
2024-04-11 09:25:27 +0300

    Create 226. Invert Binary Tree.cpp

---
+++ Leetcode/226. Invert Binary Tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void f(TreeNode* v) {
+        if (v == nullptr)
+            return;
+        swap(v->left, v->right);
+        f(v->left);
+        f(v->right);
+    }
+
+    TreeNode* invertTree(TreeNode* root) {
+        f(root);
+        return root;
+    }
+};
